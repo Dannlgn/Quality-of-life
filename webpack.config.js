2042497b530a3d7b8326173718a34a27181c8c93
Norokei <daniele.lgn96@yahoo.it>
2023-04-30 12:24:34 +0200

    Add readme and few changes on config.js

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ const config = {
       },
       {
         test: /\.(css|s[ac]ss)$/i,
-        use: [stylesHandler, "style-loader", "css-loader", 'sass-loader'],
+        use: [stylesHandler, "style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
